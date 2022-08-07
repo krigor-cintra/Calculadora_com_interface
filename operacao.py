4dbfb60b7eaae7c03b681a7ee022617ff2784a47
krigor-cintra <krigor.gcs@gmail.com>
2022-08-06 22:30:08 -0300

    Operações

---
+++ operacao.py
@@ -0,0 +1,14 @@
+def somar(valor1,valor2):
+    produto = valor1 + valor2
+    return produto
+
+def subtrair(valor1,valor2):
+    produto =valor1 - valor2
+    return produto
+
+def multiplicar(valor1, valor2):
+    produto = valor1*valor2
+
+def divisao(valor1,valor2):
+    produto=valor1/valor2
+    return produto
