a21fefcaedf1dcead7af9a6c3cc0df69e1d6087f
krigor-cintra <krigor.gcs@gmail.com>
2022-08-24 00:05:29 -0300

    24/08/2022

--- operacao.py
+++ operacao.py
@@ -13,8 +13,9 @@ def multiplicar(valor1, valor2):
 def divisao(valor1,valor2):
     produto=valor1/valor2
     return produto
-def porcentagem(valor1, valor2):
-    
+def porcentagem(valor1):
+    resultado = valor1/100
+    return resultado
 def virugla():
     return ","
 
