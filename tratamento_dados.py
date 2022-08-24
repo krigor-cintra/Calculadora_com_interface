10dc280d34932def1957579ce29c219daab5375b
krigor-cintra <krigor.gcs@gmail.com>
2022-08-23 23:58:22 -0300

    23/08/22 23:58

--- tratamento_dados.py
+++ tratamento_dados.py
@@ -6,20 +6,21 @@ def valor_digitado(valor):
 def Operação_str(str_operacao):
     if ("-" in str_operacao):
         valor = str_operacao.split("-")
-        print(str_operacao)
         return subtrair(float(valor[0]), float(valor[1]))
     elif ("+" in str_operacao):
         valor = str_operacao.split("+")
-        print(str_operacao)
         return somar(float(valor[0]), float(valor[1]))
     elif ("/" in str_operacao):
         valor = str_operacao.split("/")
-        print(str_operacao)
         return divisao(float(valor[0]), float(valor[1]))
     elif ("*" in str_operacao):
         valor = str_operacao.split("*")
-        print(str_operacao)
         return multiplicar(float(valor[0]), float(valor[1]))
+    elif ("%" in str_operacao):
+        valor = str_operacao.split("%")
+
+        return multiplicar(float(valor[0]), float(valor[1]))
+
 
 def apagar(str_operacao):
     str_operacao=str(int(float(str_operacao)))
