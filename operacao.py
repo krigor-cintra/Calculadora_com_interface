cb44fb20ada5689ac791d8cb229ccf5ce5f906c3
krigor-cintra <krigor.gcs@gmail.com>
2022-10-19 22:40:51 -0300

    Atualização

--- operacao.py
+++ operacao.py
@@ -9,13 +9,11 @@ def subtrair(valor1,valor2):
 def multiplicar(valor1, valor2):
     produto = valor1*valor2
     return produto
-
 def divisao(valor1,valor2):
     produto=valor1/valor2
     return produto
 def porcentagem(valor1):
     resultado = valor1/100
     return resultado
-def virugla():
-    return ","
+
 
